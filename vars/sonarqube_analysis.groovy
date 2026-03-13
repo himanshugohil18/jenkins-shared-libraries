b7c220ca190d31c629f46c158bfdb314950e0034
Himanshu Gohil <himanshugohil828@gmail.com>
2026-03-13 13:53:14 +0530

    Create sonarqube_analysis.groovy

---
+++ vars/sonarqube_analysis.groovy
@@ -0,0 +1,10 @@
+def call(String sonarName, String projectKey, String projectName) {
+    withSonarQubeEnv(sonarName) {
+        sh """
+        ${tool 'Sonar'}/bin/sonar-scanner \
+        -Dsonar.projectKey=${projectKey} \
+        -Dsonar.projectName=${projectName} \
+        -Dsonar.sources=.
+        """
+    }
+}
